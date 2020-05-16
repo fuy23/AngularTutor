08fbe3850bc9cd171d1c6f0029ee9ed9cb3626e7
fuy23 <fuy23@uw.edu>
2020-05-16 16:28:08 -0700

    add dishes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
+import { DishesComponent } from './dishes/dishes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    DishesComponent
   ],
   imports: [
     BrowserModule,
